16be542939406ec7435b76fc59e8cffe246fe6d3
TaoWang <watayouxiang@qq.com>
2022-08-03 14:30:12 +0800

    /

--- android/src/main/java/com/watayouxiang/android_audio_plugin/AndroidAudioPlugin.java
+++ android/src/main/java/com/watayouxiang/android_audio_plugin/AndroidAudioPlugin.java
@@ -14,21 +14,17 @@ public class AndroidAudioPlugin implements FlutterPlugin {
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
-    private MethodCallHandlerImpl handler;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
-        handler = new MethodCallHandlerImpl(binding.getApplicationContext());
+        MethodCallHandlerImpl handler = new MethodCallHandlerImpl(binding.getApplicationContext());
         channel = new MethodChannel(binding.getBinaryMessenger(), "android_audio_plugin");
         channel.setMethodCallHandler(handler);
     }
 
-
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-        handler.release();
         channel.setMethodCallHandler(null);
     }
-
 
 }
--- android/src/main/java/com/watayouxiang/android_audio_plugin/MethodCallHandlerImpl.java
+++ android/src/main/java/com/watayouxiang/android_audio_plugin/MethodCallHandlerImpl.java
@@ -2,7 +2,6 @@ package com.watayouxiang.android_audio_plugin;
 
 import android.content.Context;
 import android.media.AudioManager;
-import android.os.Build;
 
 import androidx.annotation.NonNull;
 
@@ -11,10 +10,10 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 
 public class MethodCallHandlerImpl implements MethodCallHandler {
-    private AudioManager audioManager;
+    private final Context appContext;
 
     public MethodCallHandlerImpl(@NonNull Context appContext) {
-        this.audioManager = (AudioManager) appContext.getSystemService(Context.AUDIO_SERVICE);
+        this.appContext = appContext;
     }
 
     @Override
@@ -22,50 +21,28 @@ public class MethodCallHandlerImpl implements MethodCallHandler {
         if (call.method.equals("getPlatformVersion")) {
             result.success("Android " + android.os.Build.VERSION.RELEASE);
         } else if (call.method.equals("enableSpeakerphone")) {
-            if (audioManager == null) {
-                result.error("enableSpeakerphone", "audioManager 为空", null);
-                return;
-            }
             Boolean enable = call.argument("enable");
             if (enable == null) {
                 result.error("enableSpeakerphone", "enable 为空", null);
                 return;
             }
-            enableSpeakerphone(audioManager, enable);
+            enableSpeakerphone(appContext, enable);
             result.success(null);
         } else {
             result.notImplemented();
         }
     }
 
-    private void enableSpeakerphone(@NonNull AudioManager audioManager, boolean enable) {
-        // 设置mode
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
-        } else {
-            audioManager.setMode(AudioManager.MODE_IN_CALL);
-        }
+    private void enableSpeakerphone(@NonNull Context context, boolean enable) {
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         if (enable) {
-            // 设置音量为最大
-            audioManager.setStreamVolume(AudioManager.STREAM_VOICE_CALL,
-                    audioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL),
-                    AudioManager.FX_KEY_CLICK);
-            // 打开外放
+            // 外放
+            audioManager.setMode(AudioManager.MODE_IN_CALL);
             audioManager.setSpeakerphoneOn(true);
         } else {
-            // 设置音量，解决有些机型切换后没声音或者声音突然变大的问题
-            audioManager.setStreamVolume(AudioManager.STREAM_VOICE_CALL,
-                    audioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL),
-                    AudioManager.FX_KEY_CLICK);
-            // 关闭外放
+            // 听筒
+            audioManager.setMode(AudioManager.MODE_IN_CALL);
             audioManager.setSpeakerphoneOn(false);
-        }
-    }
-
-    public void release() {
-        if (audioManager != null) {
-            audioManager.setMode(AudioManager.MODE_NORMAL);
-            audioManager = null;
         }
     }
 }
